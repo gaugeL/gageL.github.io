5d44c466e5b627e4f74cc31a30a2493e524aeada
gaugeL <63837583+gaugeL@users.noreply.github.com>
2020-04-18 04:19:39 -0500

    Make score along with a restart button

--- practice.js
+++ practice.js
@@ -2,6 +2,27 @@
 
 var bot = 0;
 var player = 0;
+var botChoice;
+var choices = ['Rock', 'Paper', 'Scissors'];
+function check() {
+    if (bot == 3){
+        write("GAME OVER, you suck dude you should get smrter than a comp bruh, like come on bro or lady ");
+        player = 0;
+        bot = 0;
+    }
+        else if (player == 3) {
+        write("GAME OVER, Oh my goodness bruv you are a genious mastermind crazy insane derryl boii. Im proud of you dawg shouts out fron indonesia");
+        player = 0;
+        bot = 0;
+    }
+}
+function restart() {
+    bot = 0;
+    player = 0;
+    check()
+    write("So you wanted a fresh start aye?");
+    score("Choose one to start agane!!");
+}
 function score(text) {
     document.getElementById("score").innerHTML = text;
 }
@@ -16,28 +37,42 @@ function selectRock() {
 
     if (botChoice == 'Rock') {
         write('tie boiiii try again');
+
     }
     else if (botChoice == choices[1]) {
         bot++;
         write('You lost deryl');
+
     }
     else {
         player++;
         write('You won, have a cookie');
+
     }
+  
+    score("Bot's score is " + bot + "         Your score is " + player);
+    check();
 }
 function selectPaper() {
     botChoice = choices[randInt(0, 2)];
 
     if (botChoice == 'Paper') {
         write("You tied but I bet you can't tie your shoes ( ͡° ͜ʖ ͡°)");
+
     }
     else if (botChoice == choices[0]) {
         write('Nice job you won');
+        player++;
+
+
     }
     else {
         write('You lost smh my head...');
+        bot++;
     }
+
+    score("Bot's score is " + bot + "         Your score is " + player);
+    check();
 }
 function selectScissors() {
     botChoice = choices[randInt(0, 2)];
@@ -47,12 +82,18 @@ function selectScissors() {
     }
     else if (botChoice == 'Rock') {
         write('You lost dummy');
+        bot++;
     }
-    else write('You won');
+    else {
+        write('You won');
+        player++;
+    }
+    
+    score("Bot's score is " + bot + "         Your score is " + player);
+    check();
 }
 
 
 
-var botChoice;
-var choices = ['Rock', 'Paper', 'Scissors'];
+
 
